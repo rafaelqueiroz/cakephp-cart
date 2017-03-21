628dcdb002ff40fbc680a7229816ae3c12f950db
Rafael Queiroz <rafaelfqf@gmail.com>
2017-03-21 13:24:27 -0300

    Adding headers in Helper files

--- tests/TestCase/View/Helper/CartHelperTest.php
+++ tests/TestCase/View/Helper/CartHelperTest.php
@@ -1,12 +1,28 @@
 <?php
 namespace Cart\Test\TestCase\View\Helper;
+/*
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * This software consists of voluntary contributions made by many individuals
+ * and is licensed under the MIT license.
+ */
 
 use Cake\TestSuite\TestCase;
 use Cake\View\View;
 use Cart\View\Helper\CartHelper;
 
 /**
- * Cart\View\Helper\CartHelper Test Case
+ * @author Rafael Queiroz <rafaelfqf@gmail.com>
  */
 class CartHelperTest extends TestCase
 {
@@ -50,6 +66,7 @@ class CartHelperTest extends TestCase
 
         $this->assertContains('<form method="post" accept-charset="utf-8" action="/cart/add">', $result);
         $this->assertContains('<input type="hidden" name="id" value="4"/>', $result);
-        $this->assertContains('</form>', $result);    
+        $this->assertContains('</form>', $result);
     }
+
 }
